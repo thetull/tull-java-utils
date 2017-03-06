5fc3d907bf1be3285dbf9ea1e34d00a12fe562be
Tull Gearreald <tull.gearreald@avant.com>
2017-03-06 16:16:18 -0600

    Added chunkifying to SQLUtils

--- src/net/tullco/tullutils/SQLUtil.java
+++ src/net/tullco/tullutils/SQLUtil.java
@@ -1,9 +1,16 @@
 package net.tullco.tullutils;
 
 import java.sql.Statement;
+import java.sql.Types;
+
+import com.opencsv.CSVWriter;
+
 import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class SQLUtil implements Closeable {
@@ -14,11 +21,15 @@ public class SQLUtil implements Closeable {
 	public SQLUtil(Connection conn){
 		this.conn=conn;
 	}
-	
+	/**
+	 * Executes a select statement and returns the ResultSet.
+	 * @param statement The statement to execute
+	 * @return The ResultSet containing the results.
+	 * @throws SQLException If the resource is closed or another error occurs.
+	 */
 	public ResultSet executeSelect(String statement) throws SQLException{
 		throwIfClosed();
 		Statement s = this.conn.createStatement();
-		s.closeOnCompletion();
 		return s.executeQuery(statement);
 	}
 
@@ -40,12 +51,137 @@ public class SQLUtil implements Closeable {
 		s.close();
 		return last_insert_id;
 	}
-
+	/**
+	 * Executes an update statement against the connection used in the constructor.
+	 * @param statement The update statement
+	 * @throws SQLException If the statement is not an update statement or there is an
+	 * error, or the resource is closed.
+	 */
 	public void executeUpdate(String statement) throws SQLException{
 		throwIfClosed();
+		if(!statement.toLowerCase().contains("update"))
+			throw new SQLException("This is not an update statement: "+statement);
 		Statement s = this.conn.createStatement();
 		s.executeUpdate(statement);
 		s.close();
+	}
+	/**
+	 * 
+	 * @param f
+	 * @param statement
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	public void getResultsAsCSV(File f, String statement) throws SQLException, IOException {
+		ResultSet rs = this.executeSelect(statement);
+		ResultSetMetaData rsmd = rs.getMetaData();
+		CSVWriter writer = FileUtils.getCSVWriter(f);
+		String[] headers = new String[rsmd.getColumnCount()];
+		for(int i=1;i<=rsmd.getColumnCount();i++){
+			headers[i-1] = rsmd.getColumnLabel(i);			
+		}
+		writer.writeNext(headers);
+		writeResultSetToCSV(writer,rs);
+	}
+	public void appendResultsToCSV(File f, String statement) throws SQLException, IOException {
+		CSVWriter writer = FileUtils.getCSVWriter(f, true);
+		try{
+			writeResultSetToCSV(writer,this.executeSelect(statement));
+		}finally{
+			writer.close();
+		}
+	}
+	/**
+	 * Will chunk up the query into smaller chunks and combines the results. Can either put chunk files in the given directory or output all results to the same file.
+	 * @param statement The SQL string you want to chunk. Make sure it has minIdString and maxIdString in the appropriate locations.
+	 * @param minIdString The string that is in the location you want to put the minimum id in the query. All instances will be replaced. 
+	 * @param maxIdString The string that is in the location you want to put the maximum id in the query. All instances will be replaced.
+	 * @param minId The smallest id that you want to check. (Inclusive)
+	 * @param maxId The largest id that you want to check. (Exclusive)
+	 * @param chunkSize The size of the chunks you want to run.
+	 * @param f The File/Directory you want to output to.
+	 * @param separateFiles True if you want the outputs to be in individual files for each chunk. False if you want the results all in one file.
+	 * @throws IOException If there is an IOException writing to the file.
+	 * @throws SQLException If there is an SQLException while running the query.
+	 */
+	public void chunkifyQueryByIdCSV(String statement, String minIdString, String maxIdString, int minId, int maxId, int chunkSize, File f, boolean separateFiles) throws SQLException, IOException{
+		int currentStart = minId;
+		int currentEnd = minId+chunkSize;
+		int maxRunId;
+		do{
+			String chunkedStatement = statement.replace(minIdString, Integer.toString(currentStart)).replace(maxIdString, Integer.toString(currentEnd));
+			System.out.println("Running "+currentStart+" to "+currentEnd+".");
+			if(separateFiles){
+				f.mkdir();
+				File chunk = new File(f.getAbsolutePath()+"/"+currentStart+"-"+currentEnd+".csv");
+				getResultsAsCSV(chunk,chunkedStatement);
+			}else{
+				if(minId==currentStart){
+					getResultsAsCSV(f,chunkedStatement);
+				}else{
+					appendResultsToCSV(f,chunkedStatement);
+				}
+			}
+			maxRunId=currentEnd;
+			currentStart+=chunkSize;
+			currentEnd+=chunkSize;
+		}while(maxRunId<maxId);
+	}
+	private void writeResultSetToCSV(CSVWriter writer, ResultSet rs) throws SQLException, IOException{
+		ResultSetMetaData rsmd = rs.getMetaData();
+		while(rs.next()){
+			String[] rowData = new String[rsmd.getColumnCount()];
+			for(int i=1; i<=rsmd.getColumnCount(); i++){
+				try{
+					if((rsmd.getColumnType(i)==Types.INTEGER) ||
+							(rsmd.getColumnType(i)==Types.SMALLINT) ||
+							(rsmd.getColumnType(i)==Types.TINYINT)){
+						rowData[i-1] = Integer.toString(rs.getInt(i));
+						continue;
+					}
+					else if(rsmd.getColumnType(i)==Types.BIGINT){
+						rowData[i-1] = Long.toString(rs.getLong(i));
+						continue;
+					}
+					else if(rsmd.getColumnType(i)==Types.DOUBLE ||
+							rsmd.getColumnType(i)==Types.FLOAT ||
+							rsmd.getColumnType(i)==Types.DECIMAL ||
+							rsmd.getColumnType(i)==Types.NUMERIC ||
+							rsmd.getColumnType(i)==Types.REAL){
+						rowData[i-1] = Double.toString(rs.getDouble(i));
+					}
+					else if(rsmd.getColumnType(i)==Types.VARCHAR ||
+							rsmd.getColumnType(i)==Types.BLOB ||
+							rsmd.getColumnType(i)==Types.CHAR){
+						rowData[i-1] = rs.getString(i);
+						continue;
+					}
+					else if(rsmd.getColumnType(i)==Types.BOOLEAN ||
+							rsmd.getColumnType(i)==(Types.BIT)){
+						rowData[i-1] = Boolean.toString(rs.getBoolean(i));
+						continue;
+					}
+					else if(rsmd.getColumnType(i)==Types.DATE){
+						rowData[i-1] = rs.getDate(i).toString();
+					}
+					else if(rsmd.getColumnType(i)==Types.TIME ||
+						rsmd.getColumnType(i)==Types.TIME_WITH_TIMEZONE){
+						rowData[i-1] = rs.getTime(i).toString();
+					}
+					else if(rsmd.getColumnType(i)==Types.TIMESTAMP ||
+							rsmd.getColumnType(i)==Types.TIMESTAMP_WITH_TIMEZONE){
+						rowData[i-1] = rs.getTimestamp(i).toString();
+					}
+					else{
+						throw new SQLException("CSV Method does not support the data type "+rsmd.getColumnTypeName(i)+".");
+					}
+				}catch(NullPointerException e){
+					rowData[i-1]="";
+				}
+			}
+			writer.writeNext(rowData);
+		}
+		rs.close();
 	}
 	/**
 	 * Throws an SQL Exception if the object is closed.
