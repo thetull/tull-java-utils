bf66ffa083bf2eb365a2c404b33810d0fab92860
Tull Gearreald <budster111@gmail.com>
2017-05-21 19:18:50 -0500

    Added equals methods and corresponding hashCode.

--- src/main/java/net/tullco/tullutils/graphutils/Edge.java
+++ src/main/java/net/tullco/tullutils/graphutils/Edge.java
@@ -36,4 +36,36 @@ public class Edge {
 	public int getDistance(){
 		return this.distance;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((destination == null) ? 0 : destination.hashCode());
+		result = prime * result + distance;
+		result = prime * result + ((origin == null) ? 0 : origin.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (destination == null) {
+			if (other.destination != null)
+				return false;
+		} else if (!destination.equals(other.destination))
+			return false;
+		if (distance != other.distance)
+			return false;
+		if (origin == null) {
+			if (other.origin != null)
+				return false;
+		} else if (!origin.equals(other.origin))
+			return false;
+		return true;
+	}
 }
